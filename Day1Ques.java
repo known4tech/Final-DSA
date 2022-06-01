618b563827f4e4dd4a85391e5c5c80c378db2004
Yatin Kumar <80054732+known4tech@users.noreply.github.com>
2022-06-01 20:07:44 +0530

    Day-1 File Added

---
+++ Day1Ques.java
@@ -0,0 +1,89 @@
+package com.yatinkumar;
+
+import java.util.Scanner;
+
+public class Day1Ques {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//Q1. Write a java program that takes two number as an input and display the product of two numbers.
+//        System.out.print("Enter value of num1: ");
+//        int num1 = sc.nextInt();
+//        System.out.print("Enter value of num2: ");
+//        int num2 = sc.nextInt();
+//        int prod = num1 * num2;
+//        System.out.println("Product of num1 & num2 is: " + prod);
+
+//Q2. Write a java program to print the area & perimeter of the circle.
+//        System.out.print("Enter value of r(radius): ");
+//        float r = sc.nextFloat();
+//        float pie = 3.14f;
+//        float area = pie * (r*r);
+//        float perimeter = 2*pie*r;
+//        System.out.println("Area of Circle: " + area);
+//        System.out.println("Perimeter of Circle: " + perimeter);
+
+//Q3. Write a java program that reads a number in inches, converts it to meters.
+//        System.out.print("Enter value of num in inches: ");
+//        float num = sc.nextFloat();
+//        float meters = num * 0.0254f;
+//        System.out.println("After converting it to meters: " + meters);
+
+//Q4. Write a java program in which take three numbers from the user and print the greatest number.
+//        System.out.print("Enter num1: ");
+//        int num1 = sc.nextInt();
+//        System.out.print("Enter num2: ");
+//        int num2 = sc.nextInt();
+//        System.out.print("Enter num3: ");
+//        int num3 = sc.nextInt();
+//
+//        if (num1 > num2 && num1 > num3) {
+//            System.out.println("Largest number is:" + num1);
+//        } else if (num2 > num3) {
+//            System.out.println("Largest number is:" + num2);
+//        } else {
+//            System.out.println("Largest number is:" + num3);
+//        }
+
+//Q5. Write a java program that takes a year from user and print whether that year is a leap year or not.
+//        System.out.print("Enter year to check if it's leap or not: ");
+//        int year = sc.nextInt();
+//
+//        if (year % 400 == 0) {
+//            System.out.println("Leap year");
+//        } else if (year % 100 == 0) {
+//            System.out.println("Not a leap year");
+//        } else if (year % 4 == 0) {
+//            System.out.println("Leap year");
+//        } else {
+//            System.out.println("Not a leap year");
+//        }
+
+//Q6. Write a java program to swap two numbers.
+//        System.out.print("Enter Num1: ");
+//        int num1 = sc.nextInt();
+//        System.out.print("Enter Num2: ");
+//        int num2 = sc.nextInt();
+//
+//        int num = num1;
+//        num1 = num2;
+//        num2 = num;
+//
+//        System.out.println("After swapping num1 is: " + num1 + " & num2 is: " + num2);
+
+//Q7. Write a java program to grade students based on their marks.
+//        System.out.print("Enter your average marks to get grade: ");
+//        int marks = sc.nextInt();
+//
+//        if (marks >= 80) {
+//            System.out.println("Grade: A");
+//        } else if ((marks < 80) && (marks >= 60)) {
+//            System.out.println("Grade: B");
+//        } else if ((marks < 60) && (marks >= 40)) {
+//            System.out.println("Grade: C");
+//        } else {
+//            System.out.println("Grade: D");
+//        }
+
+    }
+}
